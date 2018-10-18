e8273552cba4b61de6770b31f5860d41c1742ba5
Julián Gutiérrez <juliangut@gmail.com>
2018-10-18 23:57:40 +0200

    better json encoding options

--- src/Whoops/Formatter/Json.php
+++ src/Whoops/Formatter/Json.php
@@ -26,6 +26,20 @@ class Json extends JsonResponseHandler implements ExceptionFormatter
     use FormatterTrait;
 
     /**
+     * JSON encoding options.
+     * Preserve float values and encode &, ', ", < and > characters in the resulting JSON.
+     */
+    const JSON_ENCODE_OPTIONS = \JSON_UNESCAPED_UNICODE
+        | \JSON_UNESCAPED_SLASHES
+        | \JSON_PRESERVE_ZERO_FRACTION
+        | \JSON_HEX_AMP
+        | \JSON_HEX_APOS
+        | \JSON_HEX_QUOT
+        | \JSON_HEX_TAG
+        | \JSON_PARTIAL_OUTPUT_ON_ERROR
+        | \JSON_PRETTY_PRINT;
+
+    /**
      * JsonHandler constructor.
      */
     public function __construct()
@@ -61,9 +75,8 @@ class Json extends JsonResponseHandler implements ExceptionFormatter
         $addTrace = $this->addTraceToOutput();
 
         $error = $this->getExceptionData($inspector, $addTrace);
-        $options = JSON_PARTIAL_OUTPUT_ON_ERROR | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT;
 
-        echo \json_encode(['error' => $error], $options);
+        echo \json_encode(['error' => $error], static::JSON_ENCODE_OPTIONS);
 
         return Handler::QUIT;
     }
